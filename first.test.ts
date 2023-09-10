1b86e1f022d7b63035ef1b1cd826a97acb935ca5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-10 19:09:20 +0000

    11 Sept 2023, 00:39

--- first.test.ts
+++ first.test.ts
@@ -77,58 +77,6 @@ describe.only('csv parser test', () => {
   }, 100000); // increase timeout to 10 seconds);
 });
 
-describe.skip('learning test', () => {
-  it('reaads csv properly', () => {
-    const file = fs.readFileSync('./data/matches.csv', { encoding: 'utf8' });
-
-    console.log(file);
-  });
-});
-
-interface Source<T> {
-  readLine(): Promise<T | null>;
-}
-
-class CsvReader implements Source<string> {
-  private readonly readStream: fs.ReadStream;
-  private readonly parser: NodeJS.ReadWriteStream;
-  private line: string | null = null;
-
-  constructor(filePath: string) {
-    this.readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-    this.parser = parse({
-      delimiter: ',',
-      columns: true,
-    });
-    this.parser.on('readable', () => {
-      let record;
-      while ((record = this.parser.read()) !== null) {
-        console.log(this.line);
-        this.line = record as string;
-      }
-      this.parser.end();
-    });
-    this.parser.on('error', (err) => {
-      console.error(err.message);
-    });
-  }
-
-  async readLine(): Promise<string | null> {
-    if (this.line === null) {
-      await new Promise<void>((resolve) => {
-        this.parser.once('readable', () => {
-          resolve();
-        });
-      });
-    }
-    const line = this.line;
-    this.line = null;
-    return line;
-  }
-}
-
-export default CsvReader;
-
 describe.only('csv-parser', () => {
   it('wil give result', async () => {
     const sut = new CsvParser('./data/matches.csv');
@@ -140,49 +88,3 @@ describe.only('csv-parser', () => {
     console.log('The length for sure is ', result.length);
   });
 });
-
-// export class CsvReader implements Source<string> {
-//   readLine(): Promise<string | null> {
-//     throw new Error('Method not implemented.');
-//   }
-// }
-
-// import parse from 'csv-parser';
-// import fs from 'fs';
-// import readline from 'readline';
-
-// interface Source<T> {
-//   readLine(): Promise<T | null>;
-// }
-
-// class CsvReader implements Source<Record<string, any>> {
-//   private readonly readStream: fs.ReadStream;
-//   private readonly rl: readline.Interface;
-//   private readonly parser: parse.Parser;
-
-//   constructor(filePath: string) {
-//     this.readStream = fs.createReadStream(filePath);
-//     this.rl = readline.createInterface({ input: this.readStream });
-//     this.parser = parse();
-//   }
-
-//   async readLine(): Promise<Record<string, any> | null> {
-//     const line = await new Promise<string | null>((resolve) => {
-//       this.rl.once('line', (line) => {
-//         resolve(line);
-//       });
-//     });
-//     if (line === null) {
-//       return null;
-//     }
-//     this.parser.write(line);
-//     const records = await new Promise<Record<string, any>[]>((resolve) => {
-//       this.parser.on('record', (record) => {
-//         resolve([record]);
-//       });
-//     });
-//     return records[0];
-//   }
-// }
-
-// export default CsvReader;
