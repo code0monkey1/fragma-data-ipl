dc47d0b89594a10addcff711f3278c7687f6d0bc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-11 13:19:18 +0000

    11 Sept 2023, 18:49

--- src/interfaces/use-cases/teams/top-n-teams-use-case.ts
+++ src/interfaces/use-cases/teams/top-n-teams-use-case.ts
@@ -25,6 +25,8 @@ export default class TopNTeams implements TopNTeamsToFieldFirstUseCase {
   async execute(top: number, fileName: string): Promise<Map<string, number>> {
     const matches = await this.matchCsvParser.parse(fileName);
 
+    console.log('Matches', matches);
+
     const filteredMatches = this.matchFilter.filter(matches);
 
     const winCount = this.teamWinCount.getTeamWinCount(filteredMatches);
